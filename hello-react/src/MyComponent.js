bee91f0b35e406c6e92e1551baccd5d9226fb3b9
weeeeey <qser155@naver.com>
2022-09-28 18:29:07 +0900

    3.3.3 props default value

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,5 +1,7 @@
 const MyComponent = (props) => {
     return <div style={{ fontSize: 50 }}>나의 이름은 {props.name}</div>;
 };
-
+MyComponent.defaultProps = {
+    name: "기본 이름",
+};
 export default MyComponent;
