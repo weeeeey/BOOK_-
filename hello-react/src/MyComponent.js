2839572059da993974dd7cf5b1c2e299c5a54d83
weeeeey <qser155@naver.com>
2022-09-28 20:04:25 +0900

    3.3.7 클래스형 컴포넌트에서 props 사용

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,41 +1,32 @@
-// PropTypes의 종류
-// array
-// arrayOf(타입) : 특정 proptype 으로 이루어진 배열 arrayOf(PropTypes.number) 숫자로 이루어진 배열
-// bool
-// func
-// number
-// Object
-// string
-// symbol
-// node : 렌더링 할 수 있는 모든것
-// instanceof(클래스) : 특정 클래스의 인스턴스
-// oneOf(['dog','cat'])
-// oneOfType([React.PropTypes.string, React.PropTypes.number ])
-// ObjectOf(React.PropTypes.number) 객체의 모든 키 값이 인자로 주어진 객체
-// shape({name:PropTypes.string, num:PropTypes.number})
-// any
-
+import { Component } from "react";
 import PropTypes from "prop-types";
 
-const MyComponent = ({ name, favoriteNumber, children }) => {
-    return (
-        <div>
-            나의 이름은 {name} <br />
-            children 값은 {children}
-            <br />
-            제가 좋아하는 숫자는 {favoriteNumber}
-        </div>
-    );
-};
+class MyComponent extends Component {
+    static defaultProps = {
+        name: "기본 이름",
+    };
+    static propTypes = {
+        name: PropTypes.string,
+        favoriteNumber: PropTypes.number.isRequired,
+    };
+    render() {
+        const { name, favoriteNumber, children } = this.props;
+        return (
+            <div>
+                안녕하세요 제 이름은 {name} <br />
+                children 값은 {children}
+                <br />
+                제가 좋아하는 숫자는 {favoriteNumber}
+            </div>
+        );
+    }
+}
 
-MyComponent.defaultProps = {
-    name: "기본 이름",
-};
-
-MyComponent.propTypes = {
-    name: PropTypes.string,
-    favoriteNumber: PropTypes.number.isRequired,
-    // isRequired를 이용한 프로프의 타입 지정
-};
-
+// MyComponent.defaultProps = {
+//     name: "기본 이름",
+// };
+// MyComponent.propTypes = {
+//     name: PropTypes.string,
+//     favoriteNumber: PropTypes.number.isRequired,
+// };
 export default MyComponent;
