05150d67367a8e35be59038107c608e5e9dd5217
weeeeey <qser155@naver.com>
2022-09-28 17:07:13 +0900

    #2.4.8 태그 꼭 닫기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,10 +3,15 @@ import "./App.css";
 
 function App() {
     const name = "리액트";
-
-    return <div className="react">{name}</div>;
-    // 기존 css에서는 클래스를 지정할때 class = 으로 했다면
-    // jsx 에서는 className 으로 지정
+    return (
+        <div>
+            <div className="react">{name}</div>
+            <input />
+            {/* <input></input> */}
+            {/* HTML에는 input 이나 br 태그 등 닫는 태그가 없는 것들이 있음 */}
+            {/* 하지만 jsx에서는 안닫아주면 오류 뜸 */}
+        </div>
+    );
 }
 
 export default App;
