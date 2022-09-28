b5e1291dd5ee59f6c6158208df200abbef8e83e2
weeeeey <qser155@naver.com>
2022-09-28 19:44:26 +0900

    #3.3.6 PropTypes를 통한 검증

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,7 +1,11 @@
 import MyComponent from "./MyComponent";
 
 const App = () => {
-    return <MyComponent name="sdsd">리액트</MyComponent>;
+    return (
+        <MyComponent name="React" favoriteNumber={3}>
+            리액트
+        </MyComponent>
+    );
 };
 
 export default App;
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,18 +1,29 @@
-// const MyComponent = (props) => {
-//     const { name, children } = props;
-//     return (
-//         <div>
-//             나의 이름은 {name} <br />
-//             children 값은 {children}
-//             입니다.
-//         </div>
-//     );
-// };
-const MyComponent = ({ name, children }) => {
+// PropTypes의 종류
+// array
+// arrayOf(타입) : 특정 proptype 으로 이루어진 배열 arrayOf(PropTypes.number) 숫자로 이루어진 배열
+// bool
+// func
+// number
+// Object
+// string
+// symbol
+// node : 렌더링 할 수 있는 모든것
+// instanceof(클래스) : 특정 클래스의 인스턴스
+// oneOf(['dog','cat'])
+// oneOfType([React.PropTypes.string, React.PropTypes.number ])
+// ObjectOf(React.PropTypes.number) 객체의 모든 키 값이 인자로 주어진 객체
+// shape({name:PropTypes.string, num:PropTypes.number})
+// any
+
+import PropTypes from "prop-types";
+
+const MyComponent = ({ name, favoriteNumber, children }) => {
     return (
         <div>
             나의 이름은 {name} <br />
             children 값은 {children}
+            <br />
+            제가 좋아하는 숫자는 {favoriteNumber}
         </div>
     );
 };
@@ -20,4 +31,11 @@ const MyComponent = ({ name, children }) => {
 MyComponent.defaultProps = {
     name: "기본 이름",
 };
+
+MyComponent.propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired,
+    // isRequired를 이용한 프로프의 타입 지정
+};
+
 export default MyComponent;
