2257d0e442da5de319b06144dd027283b64eba4e
weeeeey <qser155@naver.com>
2022-09-29 17:21:11 +0900

    #4.2.3 임의 메서드 만들기
    이벤트에 실행할 Js 코드를 전달하는 것이 아니라, 함수 형태의 값을 전달하는것.

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -4,33 +4,38 @@ class EventPractice extends Component {
     state = {
         message: "",
     };
+
+    // 임의 메서드를 만들어서 전달하는 방법.
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleChange(e) {
+        this.setState({
+            message: e.target.value,
+        });
+    }
+
+    handleClick() {
+        alert(this.state.message);
+        this.setState({
+            message: "",
+        });
+    }
+
     render() {
         return (
             <div>
                 <h1>이벤트 연습</h1>
-                {/* input에 담은 메시지를 this.setState를 통해 state.message 값을 변경해주고 */}
-                {/* 버튼에 클릭 이벤트가 발생했을시 alert를 통해 해당 문구를 띄워줌 */}
                 <input
                     type="text"
                     name="message"
                     placeholder="아무거나 입력하세요"
                     value={this.state.message}
-                    onChange={(e) => {
-                        this.setState({
-                            message: e.target.value,
-                        });
-                    }}
+                    onChange={this.handleChange}
                 />
-                <button
-                    onClick={() => {
-                        alert(this.state.message);
-                        this.setState({
-                            message: "",
-                        });
-                    }}
-                >
-                    확인
-                </button>
+                <button onClick={this.handleClick}>확인</button>
             </div>
         );
     }
