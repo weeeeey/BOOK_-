66ddb94b443122e2f4d8e6776ad6f7ba07beedff
weeeeey <qser155@naver.com>
2022-10-03 17:08:02 +0900

    8.2 useEffect
    useEffect를 통해 렌더링 될때마다 변화를 보여줌.  return 을 추가해서 언마운트/업데이트 직전 또한 보여주기 가능

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,7 +1,21 @@
 import Info from "./info";
+import { useState } from "react";
 
 const App = () => {
-    return <Info />;
+    const [visible, setVisible] = useState(false);
+    return (
+        <div>
+            <button
+                onClick={() => {
+                    setVisible(!visible);
+                }}
+            >
+                {visible ? "숨기기" : "보이기"}
+            </button>
+            <hr />
+            {visible && <Info />}
+        </div>
+    );
 };
 
 export default App;
--- hooks-tutorial/src/info.js
+++ hooks-tutorial/src/info.js
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Info = () => {
     const [name, setName] = useState("");
     const [nickname, setNickname] = useState("");
-
+    useEffect(() => {
+        console.log("렌더링 완료");
+        console.log(name);
+        return () => {
+            console.log("cleanup");
+            console.log(name);
+        };
+        // return() : cleanup(뒷정리 함수)은 언마운트 되기 전이나 업데이특 직전 수행하고 싶은걸 보여줌
+        // 언마운트 되기 전에만 수행하고 싶으면 두번째 인자에 빈배열 넣으면 됨
+    }, [name]);
+    // useEffect 두번째 인자로 빈 배열을 넘겨주면 처음 렌더링 될때만 나오고 그 뒤로 안나옴
+    // 특정 값이 업데이트 될때만 실행하고 싶을때는 두번째 파라미터에 전달되는 배열에 검사하고 싶은 값을 넣어주면 됨
     const onChangeName = (e) => {
         setName(e.target.value);
     };
