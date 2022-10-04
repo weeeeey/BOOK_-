c27a4729539c96889a28afc63cc690e87332dba7
weeeeey <qser155@naver.com>
2022-10-04 18:19:30 +0900

    #9.3.1 classnames 라이브러리

--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -3,6 +3,7 @@ const CSSModule = () => {
     return (
         // 두개 이상의 클래스를 적용시킬려면 백틱 사용해서 이런식으로
         <div className={`${styles.wrapper} ${styles.inverted}`}>
+            {/* <div className={[style.wrapper, style.inverted].join('')}>  */}
             안녕, 나는 <span className="somthing">CSS MODULE</span>
         </div>
     );
