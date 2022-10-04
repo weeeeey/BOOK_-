109d8fb321dc4cbef99a8a68998577bd5de55e2d
weeeeey <qser155@naver.com>
2022-10-04 15:38:49 +0900

    #9.2 Sass 를 이용한 스타일링 .scss
    확장자는 .scss
    변수명은 $로
    세미클론 찍어주기

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,28 +1,16 @@
-// 일반 CSS 방식으로 스타일링 하는 방법
-// 클래스 이름은 컴포넌트명-클래스 로 지어서 중복 되는 클래스 피한다
-// eg) App-logo
+// Sass 사용하기 (Syntactically Awesome Style Sheets)
+// CSS 전처러기로 복잡한 작업 쉽게 하기 ( 재활용 , 가독성 용이)
+// Sass 두가지 확장자 .scss .sass가 있다 ( 문법이 꽤 다름)
+// .scss가 우리가 아는 css와 문법이 비슷해서 이걸 사용할거
 
 import { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import SassComponent from "./SassComponent";
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <header>
-                    <img src={logo} className="logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+            <div>
+                <SassComponent />
             </div>
         );
     }
