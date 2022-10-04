083cd2bd7e3dc7eff7ebc80e9c7f2691a3747fc7
weeeeey <qser155@naver.com>
2022-10-04 17:41:43 +0900

    #9.3.2 두개 이상 고유 클래스 적용법

--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -1,7 +1,8 @@
 import styles from "./CSSModule.module.css";
 const CSSModule = () => {
     return (
-        <div className={styles.wrapper}>
+        // 두개 이상의 클래스를 적용시킬려면 백틱 사용해서 이런식으로
+        <div className={`${styles.wrapper} ${styles.inverted}`}>
             안녕, 나는 <span className="somthing">CSS MODULE</span>
         </div>
     );
