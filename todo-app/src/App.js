4a30b1fdbe7bc275e834a0c4d277e4b984eb050a
weeeeey <qser155@naver.com>
2022-10-05 15:22:56 +0900

    #11.2 리렌더링 상황들과 최적화 하는 방법 필기

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,9 +1,22 @@
+// [리렌더링 되는 상황]
+// 1. 자신이 전달받은 props가 변경될떄
+// 2. 자신의 state가 바뀔때
+// 3. 부모 컴포넌트가 리렌더링 될때
+// 4. forceUpdate 함수가 실행될때
+
+// [현 프로그램 리렌더링 상황]
+// 할 일 항목 1을 체크하면 App 컴포넌트의 state가 변경되면서 App컴포넌트가 리렌더링
+// 부모가 리렌더링 되었으니 TodoList 리렌더링되고 그 안의 무수한 컴포넌트들도 리렌더링
+
+// [최적화 하는 방법]
+// React.memo 사용, onToggle-onRemove 함수 바뀌지 않게 하기, 불변성의 중요성
+// TodoList 컴포넌트 최적화하기
+// react-virtualized를 사용
+
 import TodoInsert from "./components/TodoInsert";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoList from "./components/TodoList";
 import { useState, useCallback, useRef } from "react";
-// props로 전달해야 할 함수를 만들 때는 useCallback 으로 감사는 것을 습관화 하기(컴포넌트 성능 최적화)
-// 새로운 객체 만들때마다 id에 +1을 해줄거임. 그런데 id 값은 렌더링 되는 정보가 아니기 때문에 useRef 사용할거
 
 function createBulkTodos() {
     const arr = [];
@@ -18,9 +31,7 @@ function createBulkTodos() {
 }
 const App = () => {
     const [todos, setTodos] = useState(createBulkTodos);
-
-    // 고유값으로 사용될 id
-    // ref를 사용하여 변수 담기(리렌더링 되면 안되니)
+    // useState(createBulkTodos()) 라고 적었다면 렌더링 할 때마다 함수 실행 시켜서 데이터가 계속해서 자동 생성 됐을거임
     const nextId = useRef(2502);
 
     const onInsert = useCallback(
@@ -39,7 +50,6 @@ const App = () => {
     const onRemove = useCallback(
         (id) => {
             setTodos(todos.filter((todo) => todo.id !== id));
-            // 내가 전달한 id와 같지 않아야 true, 같으면 false가 되어서 필터로 걸러져서 지워짐
         },
         [todos]
     );
@@ -69,6 +79,3 @@ const App = () => {
 };
 
 export default App;
-
-// 나중에 추가 할 일정 항목에 대해서는 App에서 관리함
-// App에서 useState를 이용하용 todo 상태를 정의 하고, TodoList의 props로 전달
