09e36aea645e959a7254c773039c7ba4a846f06f
weeeeey <qser155@naver.com>
2022-10-05 15:11:44 +0900

    #11 많은 데이터 렌더링

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -5,28 +5,23 @@ import { useState, useCallback, useRef } from "react";
 // props로 전달해야 할 함수를 만들 때는 useCallback 으로 감사는 것을 습관화 하기(컴포넌트 성능 최적화)
 // 새로운 객체 만들때마다 id에 +1을 해줄거임. 그런데 id 값은 렌더링 되는 정보가 아니기 때문에 useRef 사용할거
 
-const App = () => {
-    const [todos, setTodos] = useState([
-        {
-            id: 1,
-            text: "리액트 기초 알아보기",
-            checked: true,
-        },
-        {
-            id: 2,
-            text: "컴포넌트 스타일링 알아보기",
-            checked: true,
-        },
-        {
-            id: 3,
-            text: "일정 관리 앱 만들어보기",
+function createBulkTodos() {
+    const arr = [];
+    for (let i = 0; i <= 2500; i++) {
+        arr.push({
+            id: i,
+            text: `할일 ${i}`,
             checked: false,
-        },
-    ]);
+        });
+    }
+    return arr;
+}
+const App = () => {
+    const [todos, setTodos] = useState(createBulkTodos);
 
     // 고유값으로 사용될 id
     // ref를 사용하여 변수 담기(리렌더링 되면 안되니)
-    const nextId = useRef(4);
+    const nextId = useRef(2502);
 
     const onInsert = useCallback(
         (text) => {
