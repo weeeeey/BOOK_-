93f2ea3c25470432dcf60eb6db27921889bb4c7d
weeeeey <qser155@naver.com>
2022-10-05 14:22:24 +0900

    10.3.2.4 TodoInsert에서 onSubmit 이벤트 설정
    버튼의 onClick이 아닌
    폼의 onSubmit을 사용하는 이유

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -2,13 +2,25 @@ import { useState, useCallback } from "react";
 import "./TodoInsert.scss";
 import { MdAdd } from "react-icons/md";
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
     const [value, setValue] = useState("");
+
     const onChange = useCallback((e) => {
         setValue(e.target.value);
     }, []);
+
+    const onSubmit = useCallback(
+        (e) => {
+            onInsert(value);
+            setValue("");
+            e.preventDefault(); //submit 이벤트는 브라우저 새로고침을 발생시키니, 그것을 방지
+        },
+        [onInsert, value]
+    );
     return (
-        <form className="TodoInsert">
+        // onSubmit 함수가 호출되면, props로 받아 온 onInsert 함수에 현재 value 값을 파라미터로 넣어서 호출하고,
+        // 현재 value 값을 초기화한다.(onSubmit 대신에 버튼의 onClick 이벤트로도 처리 가능)
+        <form className="TodoInsert" onSubmit={onSubmit}>
             <input
                 placeholder="할 일을 입력하세요"
                 value={value}
