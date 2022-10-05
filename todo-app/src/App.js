6ef8518d8fd2019b88638d8ca506ce3e31226467
weeeeey <qser155@naver.com>
2022-10-05 16:31:12 +0900

    #11.5 useReducer를 활용한 최적화

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,11 @@
 import TodoInsert from "./components/TodoInsert";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoList from "./components/TodoList";
-import { useState, useCallback, useRef } from "react";
+import { useReducer, useCallback, useRef } from "react";
 // 현재 프로젝트에서는 todos 배열이 업데이트 되면 onToggle과 onRemove 함수도 새롭게 바뀐다.
 // 배열 상태 업뎃 과정에서 최산 todos를 참조하기 때문
 //[해결법] 1.useState의 함수형 업뎃 기능 사용  or 2. useReducer 사용
+// (둘다 성능 비슷함)
 
 function createBulkTodos() {
     const arr = [];
@@ -17,11 +18,28 @@ function createBulkTodos() {
     }
     return arr;
 }
-
-// [함수형 업데이트]: setTodos를 사용할 때, 새로운 상태를 파라미터로 넣는 대신 / 상태 업데이트를 어떻게 할 지 정의해주는 업데이트 함수
-// useCallback 두번째 파라미터에 todos를 넣을 필요 없어짐
+function todoReducer(todos, action) {
+    switch (action.type) {
+        case "INSERT":
+            return todos.concat(action.todo);
+        case "REMOVE":
+            return todos.filter((todo) => todo.id !== action.id);
+        case "TOGGLE":
+            return todos.map((todo) =>
+                todo.id === action.id
+                    ? { ...todo, checked: !todo.checked }
+                    : todo
+            );
+        default:
+            return todos;
+    }
+}
 const App = () => {
-    const [todos, setTodos] = useState(createBulkTodos);
+    const [todos, dispatch] = useReducer(
+        todoReducer,
+        undefined, //초기 상태
+        createBulkTodos //초기 상태를 만들어 주는 함수(맨 처음 렌더링 될때만 함수 호출)
+    );
     const nextId = useRef(4);
 
     const onInsert = useCallback((text) => {
@@ -30,20 +48,16 @@ const App = () => {
             text,
             checked: false,
         };
-        setTodos((todos) => todos.concat(todo));
+        dispatch({ type: "INSERT", todo });
         nextId.current += 1;
     }, []);
 
     const onRemove = useCallback((id) => {
-        setTodos((todos) => todos.filter((todo) => todo.id !== id));
+        dispatch({ type: "REMOVE", id });
     }, []);
 
     const onToggle = useCallback((id) => {
-        setTodos((todos) =>
-            todos.map((todo) =>
-                todo.id === id ? { ...todo, checked: !todo.checked } : todo
-            )
-        );
+        dispatch({ type: "TOGGLE", id });
     }, []);
     return (
         <div>
