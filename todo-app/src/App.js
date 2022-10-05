0750ea0bdbcd4d5c6e7fb4a10405090ea992c04a
weeeeey <qser155@naver.com>
2022-10-05 15:44:48 +0900

    #11.4 React.memo 사용
    컴포넌트의 props가 바뀌지 않는다면 리렌더링 하지 않도록 설정해줌

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,18 +1,3 @@
-// [리렌더링 되는 상황]
-// 1. 자신이 전달받은 props가 변경될떄
-// 2. 자신의 state가 바뀔때
-// 3. 부모 컴포넌트가 리렌더링 될때
-// 4. forceUpdate 함수가 실행될때
-
-// [현 프로그램 리렌더링 상황]
-// 할 일 항목 1을 체크하면 App 컴포넌트의 state가 변경되면서 App컴포넌트가 리렌더링
-// 부모가 리렌더링 되었으니 TodoList 리렌더링되고 그 안의 무수한 컴포넌트들도 리렌더링
-
-// [최적화 하는 방법]
-// React.memo 사용, onToggle-onRemove 함수 바뀌지 않게 하기, 불변성의 중요성
-// TodoList 컴포넌트 최적화하기
-// react-virtualized를 사용
-
 import TodoInsert from "./components/TodoInsert";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoList from "./components/TodoList";
@@ -31,7 +16,6 @@ function createBulkTodos() {
 }
 const App = () => {
     const [todos, setTodos] = useState(createBulkTodos);
-    // useState(createBulkTodos()) 라고 적었다면 렌더링 할 때마다 함수 실행 시켜서 데이터가 계속해서 자동 생성 됐을거임
     const nextId = useRef(2502);
 
     const onInsert = useCallback(
--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -6,6 +6,11 @@ import {
 import "./TodoListItem.scss";
 import cn from "classnames";
 
+import { React } from "react";
+// useMemo 사용 (컴포넌트 만들고 감싸주기만 하면 됨. export 부분에서)
+// 컴포넌트의 props가 바뀌지 않았다면, 리렌더링 하지 않도록 설정
+// 클래스 컴포넌트의 shouldComponentUpdate 라는 라이프 사이클과 용도 같음
+
 const TodoListItem = ({ todo, onRemove, onToggle }) => {
     // todo에 담겨진 것들과 변수명이 같아야 받아올 수 있음
     const { id, text, checked } = todo;
@@ -26,4 +31,4 @@ const TodoListItem = ({ todo, onRemove, onToggle }) => {
     );
 };
 
-export default TodoListItem;
+export default React.memo(TodoListItem);
