d380f8f6613a38e30ca711ae051140f8109147d9
weeeeey <qser155@naver.com>
2022-10-06 20:46:01 +0900

    #14.4 뉴스 뷰어 UI 완성

--- news-viewr/src/App.js
+++ news-viewr/src/App.js
@@ -1,31 +1,12 @@
 import { useState } from "react";
 import axios from "axios";
-import NewsItem from "./components/NewsItem";
 import NewsList from "./components/NewsList";
 
 const App = () => {
-    const [data, setData] = useState(null);
-    const onClick = async () => {
-        try {
-            const response = await axios.get(
-                "https://newsapi.org/v2/top-headlines?country=kr&category=business&apiKey=7365517dd52a4abe872de70fae706cfe"
-            );
-            setData(response.data);
-        } catch (a) {
-            console.log(a);
-        }
-    };
-
     return (
         <div>
-            <div>
-                <button onClick={onClick}>불러오기</button>
-            </div>
-            {data && (
-                <textarea rows={10} value={JSON.stringify(data, null, 2)} />
-            )}
+            <NewsList></NewsList>
         </div>
     );
 };
-
 export default App;
