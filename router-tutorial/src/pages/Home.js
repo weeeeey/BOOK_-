bfa7a9016f04c821e11d322219bfad345fd02cfd
weeeeey <qser155@naver.com>
2022-10-06 15:36:37 +0900

    # 13.3.5 Link 컴포넌트를 사용해서 페이지 이동
    <Link to="경로"> 링크 이름 <Link />

--- router-tutorial/src/pages/Home.js
+++ router-tutorial/src/pages/Home.js
@@ -1,8 +1,18 @@
+// a 태그는 브라우저에서 페이지를 새로 불러오기 때문에 SPA 위배
+// Link 컴포너는트 사용
+// LinK 컴포넌트도 a 태그를 사용하지만, 페이지를 새로 불러오는 것을 막고
+// History API를 통해 주소의 경로만 바꿔줌
+
+// <LinK to="경로"> 링크 이름 </LinK>
+
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
         <div>
             <h1>홈</h1>
             <p>가장 먼저 보여지는 페이지</p>
+            <Link to="./about">소개</Link>
         </div>
     );
 };
