b6fae9eddb4a5650eba6246539ef73d2bfa5bf28
weeeeey <qser155@naver.com>
2022-10-06 21:16:05 +0900

    #14.5 데이터 연동 && 화면에 띄우기

--- news-viewr/src/components/NewsList.js
+++ news-viewr/src/components/NewsList.js
@@ -2,6 +2,8 @@
 // 뉴스 데이터가 들어있는 배열을 컴포넌트 배열로 변환 및 렌더링
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const NewsListBlock = styled.div`
     box-sizing: border-box;
@@ -23,14 +25,37 @@ const sampleArticle = {
     urlToImage: "https://via.placeholder.com/160",
 };
 const NewsList = () => {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(null);
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get(
+                    "https://newsapi.org/v2/top-headlines?country=kr&category=business&apiKey=7365517dd52a4abe872de70fae706cfe"
+                );
+                setArticles(response.data.articles);
+            } catch (e) {
+                console.log(e);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
+
+    if (loading) {
+        return <NewsListBlock>대기중</NewsListBlock>;
+    }
+
+    if (!articles) {
+        return null;
+    }
+
     return (
         <NewsListBlock>
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
+            {articles.map((article) => (
+                <NewsItem Key={article.url} article={article} />
+            ))}
         </NewsListBlock>
     );
 };
