60bd0a1e40fe920f00a4923582318e549ecd6761
weeeeey <qser155@naver.com>
2022-10-06 17:06:04 +0900

    #13.5.1 공통 레이아웃 컴포넌트 (중첩 활용)

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,11 +1,5 @@
-// 게시글 목록 페이지에 게시글을 열었을떄,
-// 게시글 하단에 게시글 목록을 보여줘야 할 경우
-// 기존이라면  article 컴포넌트에서
-// <div>
-//   <h2>게시글{id}</h2>
-//   <Articles />
-// </div>  라고 했을 것이다.
-// 이것을 라우팅 중첩을 이용해 변경해본다.
+// 공통 레이아웃 컴포넌트
+// 중첩을 이용한 공통 요소 보여주기
 
 import { Route, Routes } from "react-router-dom";
 import About from "./pages/About.js";
@@ -13,14 +7,17 @@ import Home from "./pages/Home.js";
 import Profile from "./pages/Profile.js";
 import Articles from "./pages/Articles.js";
 import Article from "./pages/Article.js";
+import Layout from "./pages/Layout.js";
 
 const App = () => {
     return (
         <div>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/profile/:username" element={<Profile />} />
+                <Route element={<Layout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/profile/:username" element={<Profile />} />
+                </Route>
                 <Route path="/articles/" element={<Articles />}>
                     <Route path=":id" element={<Article />} />
                 </Route>
