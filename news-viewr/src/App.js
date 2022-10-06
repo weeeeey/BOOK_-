9547d303378a8211dd4b15e0bd3a869df49aad6a
weeeeey <qser155@naver.com>
2022-10-06 19:59:26 +0900

    #14.2 axios를 async로 감싸주기

--- news-viewr/src/App.js
+++ news-viewr/src/App.js
@@ -1,17 +1,22 @@
-// axois 는 가장 많이 사용되고 있는 JS HTTP 클라이언트이다.
-// [특징] : HTTP 요청을 Promise 기반으로 처리
+// async 를 사용해서 axois 처리
+// async 는 promise에 대해 보완해주는거
+// promise를 사용하는 함수 정의 하는 곳 앞에 async 붙여주고(onClick)
+// promise 함수 앞에 await 붙여주면 됨 (여기서는 axios => promise 기반이므로)
+// try catch
 import { useState } from "react";
 import axios from "axios";
 
 const App = () => {
     const [data, setData] = useState(null);
-    const onClick = () => {
-        axios
-            .get("https://jsonplaceholder.typicode.com/todos/1")
-            .then((response) => {
-                setData(response.data);
-                console.log(response.data);
-            });
+    const onClick = async () => {
+        try {
+            const response = await axios.get(
+                "https://jsonplaceholder.typicode.com/todos/1"
+            );
+            setData(response.data);
+        } catch (a) {
+            console.log(a);
+        }
     };
 
     return (
