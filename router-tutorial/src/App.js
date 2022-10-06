4cabe67198bf02303c92053de424dec6feeec37b
weeeeey <qser155@naver.com>
2022-10-06 15:29:58 +0900

    # 13 라우터 초반 설계 주석 추가
    1. yarn add 'react-router-dom'
    2. index.js => BrowserRouter 라이브러리 추가 및 App 태그 감싸기
    3. <Router path="주소 규칙" element={보여줄 컴포넌트} />

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,3 +1,8 @@
+// 1. yarn add 'react-router-dom'
+// 2. index.js => BrowserRouter 라이브러리 추가 및 App 태그 감싸기
+// 3. Route,Routes 라이브러리 소환
+// 4. <Router path="주소 규칙" element={보여줄 컴포넌트} />
+
 import { Route, Routes } from "react-router-dom";
 import About from "./pages/About.js";
 import Home from "./pages/Home.js";
