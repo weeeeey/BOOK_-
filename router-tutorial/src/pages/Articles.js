c46e07bdf2d570b635a15804cd5cbf4e07c26086
weeeeey <qser155@naver.com>
2022-10-06 17:31:00 +0900

    #NavLink를 통해 스타일링 적용시키기

--- router-tutorial/src/pages/Articles.js
+++ router-tutorial/src/pages/Articles.js
@@ -1,20 +1,51 @@
-import { Link, Outlet } from "react-router-dom";
-// Outlet 컴포넌트
-// 라우트의 children으로 들어가는 JSX 요소를 보여주는 역할
+// 링크에서 사용하는 경로===현재 라우터의 경로 일 경우
+// 특정 스타일 or CSS 클래스 적용하는 컴포넌트
+// style,className은 {isActive: boolean} 을 파라미터로 전달받는 함수 타입의 값을 전달
+
+// <NavLink style={({isActive})=>isActive? activeStyle:undefined} />
+// <NavLink className={({isActive})=>isActive? 'active':undefined} />
+
+import { NavLink, Outlet } from "react-router-dom";
 
 const Articles = () => {
+    const activeStyle = {
+        color: "green",
+        fontSize: 21,
+    };
+
     return (
         <div>
             <Outlet />
             <ul>
                 <li>
-                    <Link to="/articles/1">게시글 1</Link>
+                    <NavLink
+                        to="/articles/1"
+                        style={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }
+                    >
+                        게시글 1
+                    </NavLink>
                 </li>
                 <li>
-                    <Link to="/articles/2">게시글 2</Link>
+                    <NavLink
+                        to="/articles/2"
+                        style={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }
+                    >
+                        게시글 2
+                    </NavLink>
                 </li>
                 <li>
-                    <Link to="/articles/3">게시글 3</Link>
+                    <NavLink
+                        to="/articles/3"
+                        style={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }
+                    >
+                        게시글 3
+                    </NavLink>
                 </li>
             </ul>
         </div>
