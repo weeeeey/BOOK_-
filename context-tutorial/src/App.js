9d537be22c2ca2c618109927e0f6510c37018e0b
weeeeey <qser155@naver.com>
2022-10-07 21:50:12 +0900

    #15.3.3 색상 선택 이벤트 처리

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { ColorProvider } from "./constexts/Color";
 // default 가 아닌 컴포넌트는 import 할때 {} 로 감싸줘야 함
 import ColorBox from "./components/ColorBox";
+import SelectColors from "./components/SelectColors";
 
 const App = () => {
     return (
         <ColorProvider>
             <div>
+                <SelectColors />
                 <ColorBox />
             </div>
         </ColorProvider>
