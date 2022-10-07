140e49642001eb9c0b9f9b388c70d73e32c3d824
weeeeey <qser155@naver.com>
2022-10-07 16:57:47 +0900

    #15.2 Provider를 통한 전역변수 값 변경

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -3,9 +3,15 @@
 // 리덕스, 리액트 라우터, styled-component등은 Context API 기반으로 구현됨
 
 import ColorBox from "./components/ColorBox";
+import ColorContext from "./constexts/color";
 
+// Provider를 통해 Context의 value 변경
 const App = () => {
-    return <ColorBox></ColorBox>;
+    return (
+        <ColorContext.Provider value={{ color: "red" }}>
+            <ColorBox></ColorBox>
+        </ColorContext.Provider>
+    );
 };
 
 export default App;
