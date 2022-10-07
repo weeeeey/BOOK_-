a7808f5a1b7dddcd9f5301ba11b6cd629da68d2f
weeeeey <qser155@naver.com>
2022-10-07 14:28:47 +0900

    #14.6.1.2 App에서 카테고리 관리 및 선택 컨텐츠 색 변화

--- news-viewr/src/App.js
+++ news-viewr/src/App.js
@@ -1,11 +1,16 @@
 import Categories from "./components/Categories";
 import NewsList from "./components/NewsList";
+import { useState, useCallback } from "react";
+// App에서 카테고리 상태를 useState로 관리
+// onSelect => 카테고리 값을 업데이트 하는 함수
 
 const App = () => {
+    const [category, setCategory] = useState("alls");
+    const onSelect = useCallback((category) => setCategory(category), []);
     return (
         <div>
-            <Categories></Categories>
-            <NewsList></NewsList>
+            <Categories category={category} onSelect={onSelect} />
+            <NewsList category={category} />
         </div>
     );
 };
--- news-viewr/src/components/Categories.js
+++ news-viewr/src/components/Categories.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const CategoriesBlock = styled.div`
     display: flex;
@@ -22,6 +22,16 @@ const Category = styled.div`
     &:hover {
         color: #495057;
     }
+    ${(props) =>
+        props.active &&
+        css`
+            font-weight: 600;
+            border-bottom: 2px solid #22b8cf;
+            color: #22b8cf;
+            &:hover {
+                color: #3bc9db;
+            }
+        `}
 
     & + & {
         margin-left: 1rem;
@@ -59,11 +69,17 @@ const categories = [
     },
 ];
 
-const Categories = () => {
+const Categories = ({ onSelect, category }) => {
     return (
         <CategoriesBlock>
             {categories.map((c) => (
-                <Category key={c.name}>{c.text}</Category>
+                <Category
+                    key={c.name}
+                    active={category === c.name}
+                    onClick={() => onSelect(c.name)}
+                >
+                    {c.text}
+                </Category>
             ))}
         </CategoriesBlock>
     );
