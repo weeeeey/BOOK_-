0ec9c9b28bac7e03d961482de28d3fafd02a3c1c
weeeeey <qser155@naver.com>
2022-10-09 17:56:42 +0900

    #17.4.2 Provider 컴포넌트를 사용해서 프로젝트에 리덕스 적용 시키기

--- redux-tutorial/src/index.js
+++ redux-tutorial/src/index.js
@@ -4,11 +4,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+// 스토어 만들기
 import { createStore } from "react-redux";
 import rootReducer from "./modules";
+// 리액트에서 스토어를 사용할 수 있도록 Provider 컴포넌트로 감싸줄 용도
+import Provider from "react-redux";
 
 const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+    // 이 컴포넌트를 사용 할 때는 store을 속성으로 전달해야함
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
