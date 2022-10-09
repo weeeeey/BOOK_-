a9a7b4bac0a5eeb6347466012c4bac7dfa748461
weeeeey <qser155@naver.com>
2022-10-09 17:53:32 +0900

    #17.4.1 스토어 만들기(src/index.js에)

--- redux-tutorial/src/index.js
+++ redux-tutorial/src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 스토어를 만들고 리액트에 리덕스를 적용하는 작업은 src의 index.js 에서 이루어짐
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import { createStore } from "react-redux";
+import rootReducer from "./modules";
+
+const store = createStore(rootReducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
