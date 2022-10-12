62812e7648c5f6c1e2c71e1bab2d2fc21ba66f07
weeeeey <qser155@naver.com>
2022-10-12 21:35:01 +0900

    #17.7.1-2 useSelector, useDispatch

--- redux-tutorial/src/containers/CounterContainer.js
+++ redux-tutorial/src/containers/CounterContainer.js
@@ -1,40 +1,25 @@
-// 컨테이너 컴포넌트: 리덕스와 연동되어 있는 컴포넌트
-// 리덕스로부터 상태를 받아오고, 리덕스 스토어에 액션을 디스패치하기도 함
-
-// 컴포넌트에서 redux store 에 접근하여 원하는 상태를 받아 오고,
-// action도 dispatch 해 줄 차례
-
-// [아래의 컴포넌트를 리덕스와 연동하기 위해 connet 함수를 사용]
-// connect(mapStateToProps,mapDispatchToProps)(연동할 컴포넌트)
-// 1번 파라미터: redux store 안의 state를 컴포넌트의 props로 넘겨주기 위해 설정하는 함수
-// 2번 파라미터: action 생성 함수를 컴포넌트의 props로 넘겨주기 위해 설정하는 함수
-// connet 함수 호출 시 또 다른 함수 반환해줌. 그 뒤 연동할 컴포넌트를 파라미터로 넘어줌으로 리덕스와 연동된 컴포넌트를 만듬
-
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+// connect 대신에 useSelector 사용해보기
+// import { connect } from "react-redux";
 import Counter from "../components/Counter";
 import { increase, decrease } from "../modules/counter";
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+    const number = useSelector((state) => state.counter.number);
+    const dispatch = useDispatch();
+    // useDispatch를 통한 액션 디스패치
     return (
-        <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+        <Counter
+            number={number}
+            onIncrease={() => dispatch(increase)}
+            onDecrease={() => dispatch(decrease)}
+        />
     );
 };
 
-// const mapStateToProps = (state) => ({
-//     number: state.counter.number,
-// });
-// const mapDispatchToProps = (dispatch) => ({
-//     increase: () => {
-//         dispatch(increase());
-//     },
-//     decrease: () => {
-//         dispatch(decrease());
-//     },
-// });
+// export default connect((state) => ({ number: state.counter.number }), {
+//     increase,
+//     decrease,
+// })(CounterContainer);
 
-// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
-
-export default connect((state) => ({ number: state.counter.number }), {
-    increase,
-    decrease,
-})(CounterContainer);
+export default CounterContainer;
