aaf3e143b2723af7f2c9059f8f8810972d7385f8
weeeeey <qser155@naver.com>
2022-10-12 21:09:25 +0900

    #17.6.1 createAction 라이브러리로 counter 리덕스 생성

--- redux-tutorial/src/modules/counter.js
+++ redux-tutorial/src/modules/counter.js
@@ -1,3 +1,4 @@
+import { createAction, handleAction } from "redux-actions";
 // 액션 타입, 액션 생성 함수, 리듀서 함수를 한 곳에 묶음
 
 // 액션 타입
@@ -5,8 +6,12 @@ const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
 // 액션 생성 함수 (이 함수를 다른 파일에서 불러와 사용)
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
+
+// [createAction]을 통한 액션 생성 함수
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 // 초기 상태
 const initialState = {
@@ -14,15 +19,23 @@ const initialState = {
 };
 
 // 리듀서 함수 만들기
-function counter(state = initialState, action) {
-    switch (action.type) {
-        case INCREASE:
-            return { number: state.number + 1 };
-        case DECREASE:
-            return { number: state.number - 1 };
-        default:
-            return state;
-    }
-}
+// function counter(state = initialState, action) {
+//     switch (action.type) {
+//         case INCREASE:
+//             return { number: state.number + 1 };
+//         case DECREASE:
+//             return { number: state.number - 1 };
+//         default:
+//             return state;
+//     }
+// }
 
+// [handleActions 함수를 통한 reducer 함수 생성]
+const counter = handleAction(
+    {
+        [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+        [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+    },
+    initialState
+);
 export default counter;
